81b9fcae9907bf8f2035bc0e643ebaffa79619f3
VISHAL2005-A1 <vishalgautam1118465@gmail.com>
2026-09-18 01:38:39 +0530

    fix cors for ec2

--- backend/main.py
+++ backend/main.py
@@ -17,8 +17,8 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["http://localhost:3000"],
-    allow_origins=["http://52.207.251.20:3000"],
+   
+    allow_origins=["http://52.207.251.20:3000/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
