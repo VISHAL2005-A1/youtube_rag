b3afb1fd53dca8237da5e83306325d3483659747
Vishal tech 321 <vishalgautam1118465@gmail.com>
2026-09-18 02:14:15 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
+        "http://localhost:5173",
         "http://52.207.251.20:3000",
     ],
     allow_credentials=True,
